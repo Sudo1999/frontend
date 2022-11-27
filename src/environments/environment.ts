6971c2e12b6ace2cd9551446d5221c6bd8f475da
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2022-11-27 14:10:50 +0100

    commentaire de fichier

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,17 @@
 
 export const environment = {
   production: false,    // C'est la première ligne de environment, à laquelle on ajoute ensuite des compléments
-  //apiBaseUrl: 'http://localhost:8080/api'
-  apiBaseUrl: 'http://localhost:3000'
+  //apiBaseUrl: 'http://localhost:8080/api'   // 8080 est le port d'origine du projet Spring Boot
+  apiBaseUrl: 'http://localhost:3000'   // localhost:3000 est l'url du serveur json
+  /* A ce stade l'url http://localhost:4200/ ne fonctionne que si une des deux lignes du dessus est accessible
+  (même si le projet Spring Boot n'est pas ouvert). Le message d'erreur si les deux lignes sont commentées :
+  Compiled with problems:
+  src/app/core/services/stagiaire.service.ts:21:45 - error TS2339:
+   Property 'apiBaseUrl' does not exist on type '{ production: boolean; }'.
+  21     this.controllerBaseUrl = `${environment.apiBaseUrl}/stagiaires`;
+  (c'est en effet la ligne contenue dans le constructeur de stagiaire.service.ts)
+  Si l'une des deux lignes ('http://localhost:8080/api' ou 'http://localhost:3000') est accessible on obtient le tableau vide.
+  */
 };
 
 /*
