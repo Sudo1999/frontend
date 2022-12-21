d0e0a45a09c84540808eb0f3d74765ee59f3754d
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2022-12-21 13:47:42 +0100

    Poe form work

--- src/app/poes/components/poe-form/poe-form.component.ts
+++ src/app/poes/components/poe-form/poe-form.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { AbstractControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Poe } from 'src/app/core/models/poe';
 import { PoeService } from 'src/app/core/services/poe.service';
+import { PoeDto } from '../../dto/poe-dto';
 import { FormBuilderService } from '../../formbuilder/form-builder.service';
 
 @Component({
@@ -9,6 +13,9 @@ import { FormBuilderService } from '../../formbuilder/form-builder.service';
   styleUrls: ['./poe-form.component.scss']
 })
 export class PoeFormComponent implements OnInit {
+
+  public poeForm!: FormGroup;
+  public addMode: boolean = true;
 
   constructor(
     private router: Router,
@@ -19,7 +26,51 @@ export class PoeFormComponent implements OnInit {
 
   ngOnInit(): void {
     const data: any = this.route.snapshot.data;
-    console.log(data);
+    this.poeForm = data.form;
+    console.log(`${data}`);
+    console.log(`${this.poeForm instanceof FormGroup ? 'poeForm OK' : 'poeForm KO'}`);
+
+    this.route.url    // Observable
+      .subscribe((url: UrlSegment[]) => {
+        // Suis-je en mode update ou add ?
+        console.log(url);
+        console.log(url.filter((urlSegment: UrlSegment) => urlSegment.path === 'update').length);
+        if (url.filter((urlSegment: UrlSegment) => urlSegment.path === 'update').length) {
+          // Si le tableau n'est pas vide, length n'est pas nul => on a un update
+          console.log('Mode update');
+          this.addMode = false;
+          // On récupère le id de la POE :
+          this.poeService.findOne(parseInt(url[url.length - 1].path))
+            .subscribe((poe: Poe) => {
+              console.log(`Got ${poe.getId()} ready to update`);
+              this.poeForm = this.formBuilderService.build(poe).getForm();
+            });
+        } else {
+          console.log('Mode add');
+        }
+      });
   }
 
+  public get control(): { [key: string]: AbstractControl } {
+    return this.poeForm.controls;
+  }
+
+  public onSubmit() {
+    console.log("Le onSubmit a fonctionné : ");
+    console.log(this.poeForm.value);
+
+    const poeDto: PoeDto = new PoeDto(this.poeForm.value);
+    let subscription: Observable<any>;
+    if (this.addMode) {
+      subscription = this.poeService.add(poeDto);
+    } else {
+      // Invoke service update method
+      subscription = this.poeService.update(this.poeForm.value)
+    }
+    subscription.subscribe(() => this.goHome());
+  }
+
+  public goHome(): void {
+    this.router.navigate(['/', 'home']);
+  }
 }
--- src/app/poes/resolvers/poe.resolver.ts
+++ src/app/poes/resolvers/poe.resolver.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import {
   Router, Resolve,
   RouterStateSnapshot,
   ActivatedRouteSnapshot,
   ActivatedRoute
 } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, map, take } from 'rxjs';
+import { Poe } from 'src/app/core/models/poe';
 import { PoeService } from 'src/app/core/services/poe.service';
 import { FormBuilderService } from '../formbuilder/form-builder.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PoeResolver implements Resolve<boolean> {
+export class PoeResolver implements Resolve<FormGroup> {
 
   public constructor (
     private route: ActivatedRoute,
@@ -20,7 +22,25 @@ export class PoeResolver implements Resolve<boolean> {
     private formBuilderService: FormBuilderService
   ) {}
   
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return of(true);
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<FormGroup> {
+    const id: number = +route.paramMap.get('id')!;
+    console.log(`Got ${id} from Resolver`);
+    let poe: Poe;
+    let form: FormGroup;
+
+    if (id === 0) {
+      poe = new Poe();
+      form = this.formBuilderService.build(poe).getForm();
+      return of(form);
+    } else {
+      return this.poeService.findOne(id)
+        .pipe(
+          take(1),
+          map((mapPoe: Poe) => {
+            return this.formBuilderService.build(mapPoe).getForm();
+          })
+        )
+    }
+    //return of(true);
   }
 }
